7e0ab0b37c759d68973b7272c5a1b931f5a2169f
Ryan Laseter <proryanator@gmail.com>
2026-05-13 14:47:42 -0500

    README added, and removed invalid imports

--- wgp_fastapi/models/__init__.py
+++ wgp_fastapi/models/__init__.py
@@ -1,22 +1,3 @@
-"""
-Pydantic models for the FastAPI wrapper.
-
-Exports:
-    - Text to Image models (t2i.py)
-    - Image to Image models (i2i.py)
-    - Image to Video models (i2v.py)
-"""
-
-from wgp_fastapi.models.t2i import (
-    TextToImageRequest,
-    TextToImageResponse,
-    FluxModel,
-)
-from wgp_fastapi.models.i2i import (
-    ImageToImageRequest,
-    ImageToImageResponse,
-    I2VModel as I2VImageModel,
-)
 from wgp_fastapi.models.i2v import (
     ImageToVideoRequest,
     ImageToVideoResponse,
@@ -31,22 +12,12 @@ from wgp_fastapi.models.flux_image import (
 )
 
 __all__ = [
-    # T2I
-    "TextToImageRequest",
-    "TextToImageResponse",
-    "FluxModel",
-    # I2I
-    "ImageToImageRequest",
-    "ImageToImageResponse",
-    "I2VModel",
-    # I2V
-    "ImageToVideoRequest",
-    "ImageToVideoResponse",
-    "I2VVideoModel",
     # Flux Image
     "FluxImageRequest",
     "FluxImageResponse",
     "FluxImageModel",
     # Task Status
     "TaskStatus",
+    # Image to Video
+    "I2VVideoModel"
 ]
